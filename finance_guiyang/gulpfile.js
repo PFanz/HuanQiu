ed71c7b64c71b5e72cfaefaaf5bd4b62704aebf4
pengfeifan <pengfeifan@huanqiu.com>
2016-08-29 17:28:30 +0800

    红军游,活动页

---
+++ finance_guiyang/gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		cleanCSS = require('gulp-clean-css'),
+		babel = require('gulp-babel'),
+		uglify = require('gulp-uglify'),
+		imagemin = require('gulp-imagemin'),
+		browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+	// 默认任务代码
+});
+
+// css
+gulp.task('sass', function() {
+	return gulp.src('src/sass/*.scss')
+					.pipe(sass())
+					.pipe(autoprefixer({
+						browsers: ['last 2 versions', 'ios 6', 'android >= 4.0'],
+						remove: false
+					}))
+					.pipe(gulp.dest('src/css'))
+					.pipe(cleanCSS())
+					.pipe(gulp.dest('dist/css'))
+					.pipe(browserSync.reload({stream: true}));
+});
+
+// 压缩JS
+gulp.task('minfile', function() {
+	// jslib
+	gulp.src('src/libs/**')
+		.pipe(gulp.dest('dist/libs'));
+	// js
+	gulp.src('src/ES6/*.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('src/js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+// 压缩图片
+gulp.task('imageMin', function() {
+	gulp.src('src/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'));
+});
+
+// browser-sync服务
+gulp.task('serve', ['sass', 'minfile', 'imageMin'], function() {
+	browserSync.init({
+		server: './'
+	});
+
+	gulp.watch('src/sass/*.scss', ['sass']);
+	gulp.watch('src/ES6/*.js', ['minfile']).on('change', browserSync.reload);
+	gulp.watch('src/images/*',['imageMin']).on('change', browserSync.reload);
+	gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['minfile', 'sass', 'imageMin', 'serve']);
---
+++ finance_guiyang/src/ES6/lunbo.js
@@ -0,0 +1,78 @@
+(function(){
+	var imgWidth = 650;
+	var $focusEle = $('.focus_thumbs');
+	var len = $focusEle.find('li').length;
+
+	var autoplayFlag = 0;
+	// 定义
+	var autoplay = true;
+	var pause = true;
+	var delay = 3;
+
+	// 滚动
+	var turn = (n) => {
+		let curr = $('.dot.active').index();
+		let pos = curr + n;
+		if( pos < 0) {
+			pos = pos + len;
+		} else if( pos >= len) {
+			pos = pos - len;
+		}
+		// $focusEle.css('left', -pos * imgWidth + 'px');
+		$focusEle.animate({left: -pos * imgWidth + 'px'}, 1000);
+
+		let $currDot = $('.focus_pagination .dot').eq(pos);
+		$currDot.siblings().removeClass('active');
+		$currDot.addClass('active');
+	};
+
+	// 生成dot
+	var createDots = len => {
+		var $dotsEle = $('.focus_pagination');
+		for(var i = 0; i < len; i++) {
+			$dotsEle[0].innerHTML += '<a href="javascript:void(0);" class="dot"></a>';
+		}
+		$dotsEle.find('.dot').eq(0).addClass('active');
+		// 点击事件
+		$dotsEle.on('click', event => {
+			let target = $(event.target);
+			if(target.hasClass('dot')) {
+				clearInterval(autoplayFlag);
+				target.siblings().removeClass('active');
+				target.addClass('active');
+				// $focusEle.css('left', -target.index('.dot') * imgWidth + 'px');
+				$focusEle.animate({left: -target.index('.dot') * imgWidth + 'px'}, 500);
+				autoplayFlag = setInterval(() => {
+					turn(1);
+				}, delay * 1000);
+			}
+		});
+	};
+
+	// 初始化轮播图
+	var initFocus = len => {
+		$focusEle.width(len * imgWidth + 'px');
+		createDots(len);
+		// 自动播放
+		if(autoplay) {
+			autoplayFlag = setInterval(() => {
+				turn(1);
+			}, delay * 1000);
+			// 鼠标暂停
+			if(pause) {	
+				$focusEle.on('mouseenter', event => {
+					clearInterval(autoplayFlag);
+				});
+				$focusEle.on('mouseleave', event => {
+					autoplayFlag = setInterval(() => {
+						turn(1);
+					}, delay * 1000);
+				})
+			
+			}
+		}
+	};
+
+	initFocus(len);
+
+})();
