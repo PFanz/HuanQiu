83b426bf557741bbe70ca0e06b3e43c6736892bc
彭飞凡 <pengfeifan@huanqiu.com>
2016-10-21 18:26:15 +0800

    风尚盛典

---
+++ fengshangshengdian/src/js/main.js
@@ -0,0 +1,117 @@
+'use strict';
+
+// import { diffTime } from './time.js';
+
+(function () {
+  // 倒计时
+  setInterval(function () {
+    // 此处为2016-12-12 20:00:00
+    var time = compareTime(1481544000000);
+    // dom赋值
+    $('#txtDay').text(time.date);
+    $('#txtHours').text(time.hours);
+    $('#txtMinute').text(time.minutes);
+    $('#txtSeconds').text(time.seconds);
+  }, 1000);
+
+  // 轮播
+  new Lunbo({
+    contentElem: document.getElementById('foucsBox'),
+    delay: 5,
+    hasDot: true
+  }).init();
+
+  // 风尚榜样 风尚众誉 切换
+  $('#tab-btn-0').click(function () {
+    if ($('#tab-content-0').css('display') !== 'none') {
+      return;
+    }
+    $('.tab-btn').removeClass('honor');
+    $('#tab-content-0').show();
+    $('#tab-content-1').hide();
+  });
+
+  $('#tab-btn-1').click(function () {
+    if ($('#tab-content-1').css('display') !== 'none') {
+      return;
+    }
+    $('.tab-btn').addClass('honor');
+    $('#tab-content-1').show();
+    $('#tab-content-0').hide();
+  });
+
+  // 评审团 hover
+  $(".txtBox").mouseenter(function () {
+    $(this).animate({
+      height: '272px'
+    });
+  });
+  $(".txtBox").mouseleave(function () {
+    $(this).animate({
+      height: '63px'
+    });
+  });
+  $(".modelCon li").click(function () {
+    $(this).parents('.model').find(".modelPop").slideUp("slow");
+    var popCon = $("#" + $(this).attr("id") + "Con");
+    popCon.slideDown("slow");
+  });
+  $(".btnClose").click(function () {
+    $(this).parents('.model').find(".modelPop").slideUp("slow");
+  });
+
+  // 导航
+  var $nav0 = $('[href=#info]');
+  var $nav1 = $('[href=#standard]');
+  var $nav2 = $('[href=#vote]');
+  var $nav3 = $('[href=#review]');
+  var $nav4 = $('[href=#self]');
+  var $nav5 = $('[href=#company]');
+  var $nav6 = $('[href=#contact]');
+  $(window).scroll(function (event) {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop < 1000) {
+      if ($nav0.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav0.addClass('active');
+    } else if (scrollTop < 1500) {
+      if ($nav1.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav1.addClass('active');
+    } else if (scrollTop < 2500) {
+      if ($nav2.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav2.addClass('active');
+    } else if (scrollTop < 3300) {
+      if ($nav3.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav3.addClass('active');
+    } else if (scrollTop < 7600) {
+      if ($nav4.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav4.addClass('active');
+    } else if (scrollTop < 8500) {
+      if ($nav5.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav5.addClass('active');
+    } else {
+      if ($nav6.hasClass('active')) {
+        return;
+      }
+      $('.nav-container a').removeClass('active');
+      $nav6.addClass('active');
+    }
+  });
+})();
---
+++ fengshangshengdian/src/js/time.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// export
+var compareTime = function compareTime(time1) {
+	var time2 = arguments.length <= 1 || arguments[1] === undefined ? +new Date() : arguments[1];
+
+	var diffTime = time1 - time2;
+	diffTime = diffTime > 0 ? diffTime : -diffTime;
+	// 时间计算
+	var date = Math.floor(diffTime / (24 * 60 * 60 * 1000));
+	date = date < 10 ? '0' + date : date;
+	diffTime = diffTime - date * 24 * 60 * 60 * 1000;
+	var hours = Math.floor(diffTime / (60 * 60 * 1000));
+	hours = hours < 10 ? '0' + hours : hours;
+	diffTime = diffTime - hours * 60 * 60 * 1000;
+	var minutes = Math.floor(diffTime / (60 * 1000));
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	diffTime = diffTime - minutes * 60 * 1000;
+	var seconds = Math.floor(diffTime / 1000);
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+
+	return {
+		date: date,
+		hours: hours,
+		minutes: minutes,
+		seconds: seconds
+	};
+};
+// export default {
+// 	diffTime
+// }
